4f9d740bd32ff6183a25a5fcc301877ef0346fe2
Eric Santos <ericsantosbr2@gmail.com>
2021-07-25 23:14:41 -0300

    backup

--- gestao_produtos/views.py
+++ gestao_produtos/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import logout
+from home.models import Product
+from django.shortcuts import get_object_or_404
 
 def homepage_view(request):
-    return render(request, 'homepage.html')
+    products = Product.objects.all()
+    return render(request, 'homepage.html', {'products': products})
 
 def logout_user(request):
     logout(request)
