fa747b3818cbfc4faf11c92fe7a329694e4bc060
Eric Santos <ericsantosbr2@gmail.com>
2021-07-28 06:11:00 -0300

    Improved register page to advice for errors

--- gestao_produtos/views.py
+++ gestao_produtos/views.py
@@ -12,15 +12,21 @@ def homepage_view(request):
 def register_user(request):
     if(request.method == 'POST'):
         form = UserCreationForm(request.POST)
+        print('form.is_valid() :' + str(form.is_valid()))
+        print('form_errors: ' + str(form.errors))
         if form.is_valid():
-            form.save()
+            form_return = form.save()
+            print('form_return: ' + str(form_return))
             username = form.cleaned_data.get('username')
             raw_password = form.cleaned_data.get('password1')
             user = authenticate(username=username, password=raw_password)
             login(request, user)
             return redirect('homepage')
+        
+        else:
+            register_form = UserCreationForm()
+            return render(request, 'auth/register.html', {'form': register_form, 'errors': [form.errors]})
 
-        return redirect('homepage')
     
     else:
         register_form = UserCreationForm()
