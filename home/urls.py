643f27b2a23b21a5a1f5e806dbdfc10f59a3ab2f
Eric Santos <ericsantosbr2@gmail.com>
2021-08-16 18:19:12 -0300

    updated backslashes in the url files

--- home/urls.py
+++ home/urls.py
@@ -4,8 +4,8 @@ from .views import home_view, list_products, edit_products, add_products, delete
 urlpatterns = [
     path('', home_view),
     path('listar_produtos/', list_products, name='list_products'),
-    path('editar_produtos/<product_id>', edit_products, name='edit_products'),
+    path('editar_produtos/<product_id>/', edit_products, name='edit_products'),
     path('adicionar_produtos/', add_products, name='add_products'),
-    path('apagar_produtos/<product_id>', delete_products, name='delete_products')
+    path('apagar_produtos/<product_id>/', delete_products, name='delete_products')
 
 ]
