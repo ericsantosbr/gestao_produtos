13567eaaa70068263c2ae7a7a268cf52b00865ac
Eric Santos <ericsantosbr2@gmail.com>
2021-08-23 11:47:54 -0300

    refactored permissions for changing products and added a 403 page

--- home/views.py
+++ home/views.py
@@ -1,7 +1,7 @@
 import os
 from django.conf import settings
 from django.shortcuts import render, get_object_or_404, redirect
-from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import login_required, permission_required
 from .models import Product
 from .forms.edit_product_form import EditProduct
 
@@ -15,8 +15,6 @@ def home_view(request):
 @login_required
 def list_products(request):
     lista = Product.objects.filter(owner=request.user)
-    # lista2 = Product(owner=request.user)
-    # print('Lista: '+ str(lista2) + '\nObjects: ' + str(lista2.objects))
     return render(request, 'home_list_objects.html', {'data': lista})
 
 
@@ -40,7 +38,7 @@ def add_products(request):
         return redirect('list_products')
     return render(request, 'home_add_object.html', {'form': form})
 
-@login_required
+@permission_required('home.delete_product', raise_exception=True)
 def delete_products(request, product_id):
     product = get_object_or_404(Product, pk=product_id)
 
@@ -49,3 +47,14 @@ def delete_products(request, product_id):
         return redirect('list_products')
 
     return render(request, 'home_delete_object.html', {'data': product})
+
+@permission_required('home.change_product', raise_exception=True)
+def deactivate_products(request, product_id):
+    product:Product = get_object_or_404(Product, pk=product_id)
+
+    if request.POST:
+        product.active = False
+        product.save()
+        return redirect('homepage')
+
+    return render(request, 'home_deactivate_object.html', {'data': product})
